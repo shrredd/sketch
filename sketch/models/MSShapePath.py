358af89727d03b8a70d77365f7ad8ea0ff205fde
Shravan Reddy <shravan@dropbox.com>
2015-03-24 17:13:44 -0700

    remove old render code from MSShapePath

--- sketch/models/MSShapePath.py
+++ sketch/models/MSShapePath.py
@@ -1,5 +1,4 @@
 from sketch.models.MSCurvePoint import MSCurvePoint
-from sketch.utils import pairwise
 
 
 class MSShapePath(object):
@@ -26,20 +25,3 @@ class MSShapePath(object):
                     isClosed: {isClosed}".format(
                         points=self.points,
                         isClosed=self.isClosed)
-
-    def render(self, surface):
-        import gizeh as gz
-
-        for curr_pt, next_pt in pairwise(self.points):
-            pt_list = [(curr_pt.point.x, curr_pt.point.y),
-                       (next_pt.curveFrom.x, next_pt.curveFrom.y),
-                       (next_pt.curveTo.x, next_pt.curveTo.y),
-                       (next_pt.point.x, next_pt.point.y)]
-
-            pt_list = [(x * surface.width, y * surface.height) for (x, y) in pt_list]
-
-            print "Point list: %s" % pt_list
-            curve = gz.bezier_curve(pt_list,
-                                    fill=(0.5, 0.5, 0.5),
-                                    stroke_width=1)
-            curve.draw(surface)
